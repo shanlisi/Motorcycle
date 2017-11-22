963ec2e51c176ecc3af27e585d6ded05c075be91
shixx106 <shixxx106@163.com>
2017-11-22 15:35:06 +0800

    20171122

--- src/components/Tab/Tab.js
+++ src/components/Tab/Tab.js
@@ -1,14 +1,15 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import './Tab.less';
 import {NavLink} from 'react-router-dom'
 import cookie from '../../containers/App/cookie';
-export default class Tab extends Component{
-    render(){
+
+export default class Tab extends Component {
+    render() {
         return (
             <div className='my-tab'>
-                <NavLink to='/' exact>  <i className='iconfont  icon-createtask_fill'></i>  <span>资讯</span> </NavLink>
-                <NavLink to='/list'>  <i className='iconfont  icon-gouwuche'></i><span>商城</span></NavLink>
-                <NavLink to='/shoppingCart'><i className='iconfont  icon-ft-motorcycle'></i><span>购物车</span></NavLink>
+                <NavLink to='/' exact> <i className='iconfont  icon-createtask_fill'></i> <span>资讯</span> </NavLink>
+                <NavLink to='/list'> <i className='iconfont icon-ft-motorcycle'></i><span>商城</span></NavLink>
+                <NavLink to='/shoppingCart'><i className='iconfont icon-gouwuche'></i><span>购物车</span></NavLink>
                 <NavLink to='/mine'><i className='iconfont  icon-personal-copy'></i><span>我的</span></NavLink>
             </div>
         )
