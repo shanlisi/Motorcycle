34b023b08b620f3b5da2794b19ff7b174ab1aaf8
lismallbai <lismallbai@163.com>
2017-11-24 15:34:16 +0800

    完善个人中心

--- src/containers/Mine/Mine.js
+++ src/containers/Mine/Mine.js
@@ -4,6 +4,7 @@ import './Mine.less';
 import MyHeader from "../../components/MyHeader/MyHeader";
 import cookie from  '../App/cookie'
 import {myGet} from '../../api/index'
+import LoadPicture from "../../components/LoadPicture/index";
 export default class Mine extends Component {
     constructor() {
         super();
@@ -17,7 +18,6 @@ export default class Mine extends Component {
        myGet('/logout').then(res=>{
            console.log(res);
        })
-        //cookie.remove('USER')
     };
     handleTack = () => {
         let userID = JSON.parse(cookie.get('USER')).userId;
@@ -37,28 +37,11 @@ export default class Mine extends Component {
             window.location.href = 'http://localhost:8555/#/login';
         }
     };
-    aa = () => {
-        let userID = JSON.parse(cookie.get('USER')).userId;
-        console.log(cookie.get('USER'));
-        if (userID) {
-            myGet('/user/' + userID).then(res => {
-                if (res.code == 0) {
-                    if (res.login) {
-                        this.setState({userInfo: res.userInfo});
-                        console.log(res.userInfo);
-                        console.log(this.state.userInfo);
-                    } else {
-                        alert(res.error);
-                    }
-                }
-            });
-        }
-    };
 
     componentDidMount() {
         this. handleTack();
     }
-
+    // [{"name":"libai","phone":"15010015294","dizhi":"北京昌平区回龙观东大街3号楼珠峰培训3楼快递存放处"}]
     render() {
         return (
             <div className="mine-bg">
@@ -66,8 +49,9 @@ export default class Mine extends Component {
                 <div className='my-container'>
                     <div className="mine-header">
                         <div className="mine-left">
-                            <img src={require('../../images/mine-header.jpg')}/>
-                            <span>未认证</span>
+                            <LoadPicture/>
+                            {/*<img src={require('../../images/mine-header.jpg')}/>*/}
+                            <span>点击图片</span>
                         </div>
                         <div className="mine-cen">
                             <p>{this.state.userInfo.userName ? this.state.userInfo.userName : "登录"}</p>
@@ -78,10 +62,6 @@ export default class Mine extends Component {
                         <div className="mine-right">
                             <Link to='/login' className="mine-out" onClick={this.handleOut}
                                   >退出</Link>
-
-                            <div className="mine-pro" onClick={this.handleTack} >
-                                <i className="iconfont icon-arrow_right_icon"></i>
-                            </div>
                         </div>
 
                     </div>
@@ -94,14 +74,22 @@ export default class Mine extends Component {
                             </Link>
                         </li>
                         <li>
-                            <Link to="/sub">
+                            <Link to={{
+                                pathname: `/sub`,
+                                state: 'hello',
+                                params: {userinfo: this.state.userInfo}
+                            }} onClick={this.handleTack}>
                                 <i className="iconfont icon-dingdan"></i>
                                 <span>我的订单</span>
                                 <i className="iconfont icon-arrow-right"></i>
                             </Link>
                         </li>
                         <li>
-                            <Link to="/map">
+                            <Link to={{
+                                pathname: `/map`,
+                                state: 'hello',
+                                params: {userinfo: this.state.userInfo}
+                            }} onClick={this.handleTack}>
                                 <i className="iconfont icon-dizhi"></i>
                                 <span>收货地址</span>
                                 <i className="iconfont icon-arrow-right"></i>
@@ -113,7 +101,7 @@ export default class Mine extends Component {
                                 pathname: `/profile`,
                                 state: 'hello',
                                 params: {userinfo: this.state.userInfo}
-                            }}>
+                            }} onClick={this.handleTack}>
                                 <i className="iconfont icon-ziliao"></i>
                                 <span>我的资料</span>
                                 <i className="iconfont icon-arrow-right"></i>
